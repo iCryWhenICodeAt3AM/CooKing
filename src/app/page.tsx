f272ff44056799892e19715bb4f333fda25edc8f
Rey Longno <rey.longno@outlook.com>
2025-05-06 08:57:56 +0800

    fixed sitemap issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,17 +117,6 @@ export default function Home() {
               </svg>
               Contribute on GitHub
             </a>
-            <a 
-              href="/sitemap" 
-              className="flex items-center text-white/70 hover:text-white transition-colors text-sm group"
-              aria-label="View Sitemap"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="mr-2 group-hover:scale-110 transition-transform" viewBox="0 0 16 16">
-                <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
-                <path d="M3 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm0-5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-1z"/>
-              </svg>
-              Sitemap
-            </a>
           </div>
         </div>
         
